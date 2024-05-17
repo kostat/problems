377c7fc1484c7bf847511128703570733a0f6999
Kosta Triger <kostat@gmail.com>
2024-05-18 01:16:50 +0300

    adding tests

--- leetcode/src/main/java/org/example/LengthOfLongestSubstring.java
+++ leetcode/src/main/java/org/example/LengthOfLongestSubstring.java
@@ -14,11 +14,24 @@ public class LengthOfLongestSubstring {
             if (prev == null || prev < start) {
                 continue;
             }
-            
+
             result = Math.max(result, i - start);
             start = prev + 1;
         }
 
         return Math.max(result, s.length() - start);
+//        Set<Character> set = new HashSet<>();
+//        int result = 0, i = 0, j = 0;
+//
+//        while (i < s.length() && j < s.length()) {
+//            if (set.contains(s.charAt(j))) {
+//                set.remove(s.charAt(i++));
+//            } else {
+//                set.add(s.charAt(j++));
+//                result = Math.max(result, j - i);
+//            }
+//        }
+
+//        return result;
     }
 }
--- leetcode/src/test/java/org/example/LengthOfLongestSubstringTest.java
+++ leetcode/src/test/java/org/example/LengthOfLongestSubstringTest.java
@@ -15,4 +15,39 @@ class LengthOfLongestSubstringTest {
         assertEquals(3, LengthOfLongestSubstring.lengthOfLongestSubstring("dvdf"));
         assertEquals(2, LengthOfLongestSubstring.lengthOfLongestSubstring("abba"));
     }
+
+    @Test
+    void lengthOfLongestSubstring_allUniqueCharacters() {
+        assertEquals(4, LengthOfLongestSubstring.lengthOfLongestSubstring("abcd"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_allSameCharacters() {
+        assertEquals(1, LengthOfLongestSubstring.lengthOfLongestSubstring("aaaaa"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_emptyString() {
+        assertEquals(0, LengthOfLongestSubstring.lengthOfLongestSubstring(""));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_singleCharacter() {
+        assertEquals(1, LengthOfLongestSubstring.lengthOfLongestSubstring("a"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_substringAtStart() {
+        assertEquals(2, LengthOfLongestSubstring.lengthOfLongestSubstring("aab"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_substringAtEnd() {
+        assertEquals(2, LengthOfLongestSubstring.lengthOfLongestSubstring("baa"));
+    }
+
+    @Test
+    void lengthOfLongestSubstring_substringInMiddle() {
+        assertEquals(2, LengthOfLongestSubstring.lengthOfLongestSubstring("baab"));
+    }
 }
